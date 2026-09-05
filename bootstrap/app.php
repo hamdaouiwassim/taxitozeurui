deecfe1e1e6cedc139e374476d3515d587c09fff
hamdaouiwassim <hamdaouiwassim@gmail.com>
2026-09-05 19:09:38 +0100

    fix multi language button mobile

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
+require_once __DIR__.'/../app/Helpers/localize.php';
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
--- resources/views/partials/lang-switcher.blade.php
+++ resources/views/partials/lang-switcher.blade.php
@@ -14,4 +14,20 @@
         $url = $otherLocale === 'fr' ? url('/fr') : url('/');
     }
 @endphp
-<a href="{{ $url }}" class="lang-switch-link" title="@lang('Switch to :locale', ['locale' => $altLabel])">{{ $altLabel }}</a>
+<a href="{{ $url }}" class="lang-switch-link" title="@lang('Switch to :locale', ['locale' => $altLabel])" aria-label="@lang('Switch to :locale', ['locale' => $altLabel])">
+  @if ($otherLocale === 'fr')
+  <svg class="lang-flag" viewBox="0 0 60 36" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <rect width="20" height="36" fill="#0055A4"/>
+    <rect x="20" width="20" height="36" fill="#FFFFFF"/>
+    <rect x="40" width="20" height="36" fill="#EF4135"/>
+  </svg>
+  @else
+  <svg class="lang-flag" viewBox="0 0 60 36" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path d="M0,0 v36 h60 v-36 z" fill="#012169"/>
+    <path d="M0,0 60,36 M60,0 0,36" stroke="#FFFFFF" stroke-width="6" fill="none"/>
+    <path d="M0,0 60,36 M60,0 0,36" stroke="#C8102E" stroke-width="4" fill="none"/>
+    <path d="M30,0 v36 M0,18 h60" stroke="#FFFFFF" stroke-width="10" fill="none"/>
+    <path d="M30,0 v36 M0,18 h60" stroke="#C8102E" stroke-width="6" fill="none"/>
+  </svg>
+  @endif
+</a>
